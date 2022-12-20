6994394890af619301ec9719ef9a4d7153909b9f
ekin <your_email@example.com>
2022-12-20 16:35:54 +0300

    sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 import { defineNuxtConfig } from "nuxt";
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+import posts from "./content/contentrain/posts/posts.json";
+
+const generateRoutes = () => {
+  return (posts as Array<any>).map((p) => {
+    return { url: `/posts/${p.slug}`, lastmod: new Date(1671475385857) };
+  });
+};
+
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/content"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/content", "@nuxtjs/sitemap"],
   css: ["@/assets/css/main.css", "remixicon/fonts/remixicon.css"],
+  sitemap: {
+    gzip: true,
+    hostname: "https://1ek.in",
+  },
+  generate: {
+    routes: generateRoutes(),
+  },
 });
