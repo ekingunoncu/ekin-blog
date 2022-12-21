6e24717bba3c0062ff6abdd046446607cdcc8937
ekin <your_email@example.com>
2022-12-21 10:00:36 +0300

    trailingSlash: false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@nuxt/content"],
   css: ["@/assets/css/main.css", "remixicon/fonts/remixicon.css"],
-  baseURL: "/"
+  router: {
+    trailingSlash: false
+  }
 });
