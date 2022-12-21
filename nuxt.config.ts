3a73873dc0ae17711873c8815130b69e6e7daba7
ekin <your_email@example.com>
2022-12-21 08:55:10 +0300

    BASE URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ import { defineNuxtConfig } from "nuxt";
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@nuxt/content"],
   css: ["@/assets/css/main.css", "remixicon/fonts/remixicon.css"],
+  baseURL: ""
 });
